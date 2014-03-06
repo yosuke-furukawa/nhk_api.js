778b990b1b3e9b7a791b4d6dd5d2fbbbf664007f
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-03-06 09:21:24 +0900

    Add japanize time zone

---
+++ test/base.js
@@ -0,0 +1,29 @@
+var Base = require('../lib/base');
+var test = require('tape');
+test(' format time 2014-03-05', function (t) {
+  var base = new Base();
+  var now = new Date();
+  now.setUTCFullYear(2014);
+  now.setUTCMonth(3 - 1);
+  now.setUTCDate(5);
+  now.setUTCHours(0);
+
+  base._jpnize(now);
+  var date = base._formatDate(now);
+  t.equal(date, "2014-03-05");
+  t.end();
+});
+
+test(' format time 2014-03-06', function (t) {
+  var base = new Base();
+  var now = new Date();
+  now.setUTCFullYear(2014);
+  now.setUTCMonth(3 - 1);
+  now.setUTCDate(5);
+  now.setUTCHours(22);
+
+  base._jpnize(now);
+  var date = base._formatDate(now);
+  t.equal(date, "2014-03-06");
+  t.end();
+});
