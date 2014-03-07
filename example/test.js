d64e8d1a4c09bf89c28b83cff03631f8efd903a7
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-03-07 09:05:51 +0900

    Fix examples

--- example/test.js
+++ example/test.js
@@ -1,6 +1,6 @@
 var NHK = require("nhk_api");
-var nhk = new NHK("5K77uQA6JQi8GGWGHjnYdE9C0pcKAiQb");
+var nhk = new NHK("Ax01o1yUnUP8fTARBWP4rJ3GAY2CxoAY");
 
-nhk.list.get("130", "g1", function(err, result){
+nhk.list.get("130", "g1", "tomorrow", function(err, result){
      console.log(result);
 });
